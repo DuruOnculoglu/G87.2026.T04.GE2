d76501d103bf5decdb2a8027d1749bfa93136156
Ryan Michael Nuera <ryanmichaelnuera@Ryans-MacBook-Pro-9.local>
2026-02-22 18:27:55 +0100

    started writing register project function, not finished and has errors

--- G87.2026.T04.EG2-main/src/main/python/uc3m_consulting/enterprise_manager.py
+++ G87.2026.T04.EG2-main/src/main/python/uc3m_consulting/enterprise_manager.py
@@ -1,3 +1,7 @@
+from .enterprise_management_exception import EnterpriseManagementException
+from .enterprise_project import EnterpriseProject
+from datetime import datetime
+
 """Module """
 
 class EnterpriseManager:
@@ -7,11 +11,30 @@ class EnterpriseManager:
 
     def register_project(self, company_cif: str, project_acronym: str, project_description: str,
                          department: str, date: str, budget: float):
+        if not self.validate_cif():
+            raise EnterpriseManagementException("Invalid Cif")
+
+        if len(project_acronym) > 10 or len(project_acronym) < 5:
+            raise EnterpriseManagementException("Invalid Project Acronym Length")
+        if not project_acronym.isalnum:
+            raise EnterpriseManagementException("Invalid Project Acronym Characters")
+
+        if not len(project_description) <= 30 or len(project_description) >= 10:
+            raise EnterpriseManagementException("Invalid Project Description Length")
+
+        try:
+            datetime.strptime(date, "%d/%m/%Y").date()
+        except ValueError: raise EnterpriseManagementException("Invalid Date")
+        if date < datetime.date.today():
+            raise EnterpriseManagementException("Invalid Date")
+
         if budget < 50000:
             raise EnterpriseManagementException("Budget too low")
         objProject = enterprise_project(company_cif, project_acronym, project_description,
                              department, date, budget)
         return objProject.project_id
+
+
 
     @staticmethod
     def validate_cif(cif: str):
