a4eba19be4859c76a6b05dfb424c59fa8692ba3d
Duru Onculoglu <100591206@alumnos.uc3m.es>
2026-02-23 15:49:56 -0600

    added some invalid test cases

--- G87.2026.T04.EG2-main/src/unittest/python/test_register_project_tests.py
+++ G87.2026.T04.EG2-main/src/unittest/python/test_register_project_tests.py
@@ -35,5 +35,49 @@ class MyTestCase(unittest.TestCase):
         "Logistics", "31/12/2026", 1000000.00)
         self.assertTrue(obj,"af6c439801893f25b2d1d023ea9fe470" )
 
+    # INVALID TEST CASES
+    # ECNV5
+    def test_tc5(self):
+        manager = EnterpriseManager()
+        obj = manager.register_project("12345678", "PR001", "car automatic development",
+                                       "HR", "1/1/2027", 50000.00)
+        # self.assertTrue(obj, "")
+
+    # ECNV3, BLNV1
+    def test_tc6(self):
+        manager = EnterpriseManager()
+        obj = manager.register_project("B1234567", "PR0000002", "valid texts",
+                                       "Legal", "31/12/2026", 75000.00)
+        # self.assertTrue(obj, "")
+
+    # ECNV4, BLNV2
+    def test_tc7(self):
+        manager = EnterpriseManager()
+        obj = manager.register_project("B123456789", "PR001", "valid texts",
+                                           "Legal", "31/12/2026", 75000.00)
+        # self.assertTrue(obj, "")
+
+    # ECNV4? ==> CHECK THIS IN THE EXCEL SHEET - THERE IS A MISTAKE WITH THE NUMBERING - FIX IT
+    def test_tc8(self):
+        manager = EnterpriseManager()
+        obj = manager.register_project("712345678", "PR001", "valid texts",
+                                       "Legal", "31/12/2026", 50000.00)
+        # self.assertTrue(obj, "")
+
+    # ECNV6, BLNV3
+    def test_tc9(self):
+        manager = EnterpriseManager()
+        obj = manager.register_project("B12345678", "PR07", "car automatic development",
+                                       "HR", "31/12/2026", 50000.00)
+        # self.assertTrue(obj, "")
+
+    # ECNV6, BLNV4
+    def test_tc10(self):
+        manager = EnterpriseManager()
+        obj = manager.register_project("B12345678", "PRCF538FG07", "car automatic development",
+                                       "HR", "31/12/2026", 50000.00)
+        # self.assertTrue(obj, "")
+
+
 if __name__ == '__main__':
     unittest.main()
